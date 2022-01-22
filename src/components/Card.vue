d3239560260f9fe4dcc4aeb70e0eebcc40e203aa
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-01-22 01:45:35 +0100

    modifica gestione bandiere

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,6 +3,7 @@
   @mouseover="activeHover"
   @mouseleave="resetHover"
   >
+
     <!-- di base mostra immagine poster -->
     <img
     v-if="!hover"
@@ -27,7 +28,9 @@
       </div>
       <div class='card-detail card-language'>
       <!-- ottieni immagine della bandiera, se presente, altrimenti scrive iniziale lingua -->
-        <img :src="getFlag(card.language)" :alt="card.language">
+        <strong>Language: </strong>
+        <!-- <img :src="getFlag(card.language)" :alt="card.language"> -->
+        {{getFlag(card.language)}}
       </div>
       <div class='card-detail card-vote' v-if="card.vote > 0">
         <!-- stelle gialle-->
@@ -66,24 +69,33 @@ export default {
   data() {
     return {
       hover: false,
-      flags: {
-        it: 'https://img.icons8.com/office/italy.png',
-        en: 'https://img.icons8.com/color/usa.png',
-        fr: 'https://img.icons8.com/color/france.png',
+      java_flags: {
+        de: '\uD83C\uDDE9\uD83C\uDDEA',
+        fr: '\uD83C\uDDEB\uD83C\uDDF7',
+        it: '\uD83C\uDDEE\uD83C\uDDF9',
+        nl: '\uD83C\uDDF3\uD83C\uDDF1',
+        en: '\uD83C\uDDFA\uD83C\uDDF8',
       },
+
     };
   },
   props: {
     card: Object,
   },
   methods: {
-    // ottieni bandiera da lingua, se presente nei data
+    // ottieni bandiera da lingua, se presente nei data, altrimenti ottieni lingua
     getFlag(lan) {
-      if (this.flags[lan]) {
-        return this.flags[lan];
+      if (this.java_flags[lan]) {
+        return this.java_flags[lan];
       }
-      return '';
+      return lan;
     },
+    // getFlag(lan) {
+    //   if (this.flags[lan]) {
+    //     return this.flags[lan];
+    //   }
+    //   return '';
+    // },
     // funzioni gestione hover
     activeHover() {
       this.hover = true;
@@ -112,15 +124,15 @@ export default {
   }
   .hover-container {
     .card-detail {
-      font-size: 1.15rem;
+      font-size: 1.1rem;
+      padding-bottom: .35rem;
+      width: calc($cardWidth - 2rem);
+      //titolo di ogni card-detail
       strong {
-      padding-right: .5rem;
+        font-size: 1.22rem;
+        padding-right: .5rem;
       }
-      .card-language {
-        img {
-          margin: 0 auto;
-        }
-      }
+      //paragrafo contenente descrzione 'overview'
       p {
         display: inline;
       }
@@ -134,6 +146,10 @@ export default {
       path {
         fill:grey;
       }
+    }
+    &.card-language {
+      @include flex($ali:center);
+      text-align: center;
     }
    }
   }
